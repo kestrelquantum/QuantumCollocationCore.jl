e8ee93dc6c0f3050cf763c18e959599e65708ae6
Jack Champagne <jchampag@andrew.cmu.edu>
2024-12-02 20:01:26 -0500

    add basic literate pages

--- docs/make.jl
+++ docs/make.jl
@@ -1,23 +1,57 @@
 using QuantumCollocationCore
 using Documenter
+using Literate
 
-DocMeta.setdocmeta!(QuantumCollocationCore, :DocTestSetup, :(using QuantumCollocationCore); recursive=true)
+push!(LOAD_PATH, joinpath(@__DIR__, "..", "src"))
+
+pages = [
+    "Home" => "index.md",
+    "Lib" => "lib.md",
+    "Explanation" => "generated/explanation.md",
+]
+
+format = Documenter.HTML(;
+    prettyurls=get(ENV, "CI", "false") == "true",
+    canonical="https://kestrelquantum.github.io/QuantumCollocationCore.jl",
+    edit_link="main",
+    assets=String[],
+    mathengine = MathJax3(Dict(
+        :loader => Dict("load" => ["[tex]/physics"]),
+        :tex => Dict(
+            "inlineMath" => [["\$","\$"], ["\\(","\\)"]],
+            "tags" => "ams",
+            "packages" => [
+                "base",
+                "ams",
+                "autoload",
+                "physics"
+            ],
+        ),
+    )),
+)
+
+src = joinpath(@__DIR__, "src")
+lit = joinpath(@__DIR__, "literate")
+
+lit_output = joinpath(src, "generated")
+
+for (root, _, files) ∈ walkdir(lit), file ∈ files
+    splitext(file)[2] == ".jl" || continue
+    ipath = joinpath(root, file)
+    opath = splitdir(replace(ipath, lit=>lit_output))[1]
+    Literate.markdown(ipath, opath)
+end
+
 
 makedocs(;
     modules=[QuantumCollocationCore],
     authors="Aaron Trowbridge <aaron.j.trowbridge@gmail.com> and contributors",
     sitename="QuantumCollocationCore.jl",
-    format=Documenter.HTML(;
-        canonical="https://aarontrowbridge.github.io/QuantumCollocationCore.jl",
-        edit_link="main",
-        assets=String[],
-    ),
-    pages=[
-        "Home" => "index.md",
-    ],
+    format=format,
+    pages=pages,
 )
 
 deploydocs(;
-    repo="github.com/aarontrowbridge/QuantumCollocationCore.jl",
+    repo="github.com/kestrelquantum/QuantumCollocationCore.jl",
     devbranch="main",
 )
